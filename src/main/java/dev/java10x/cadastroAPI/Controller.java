ff647dd689cd655f2cb78e5d18cfb0c24130f6bd
Gustavo <l.gustavo.gallo20@gmail.com>
2025-12-06 02:21:37 -0300

    Criado o Controller

---
+++ src/main/java/dev/java10x/cadastroAPI/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.cadastroAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Seja bem vindo!";
+    }
+}
