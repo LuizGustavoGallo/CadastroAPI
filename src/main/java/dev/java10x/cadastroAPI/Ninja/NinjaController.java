888d955df999c17fa787c75fc2d4edf0d26fb171
Gustavo <l.gustavo.gallo20@gmail.com>
2025-12-18 15:59:58 -0300

    feat: Estruturação do Ninja Controller com o mock de dados

--- src/main/java/dev/java10x/cadastroAPI/Ninja/NinjaController.java
+++ src/main/java/dev/java10x/cadastroAPI/Ninja/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.cadastroAPI.Ninja;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Seja bem vindo!";
+    }
+
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Exibindo todos os ninjas";
+    }
+
+    // Mostrar por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjasPorIds() {
+        return "Mostrar ninja por ID";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar ninja por ID";
+    }
+
+    // Deleter Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarId() {
+        return "Ninja deletado por ID";
     }
 }
