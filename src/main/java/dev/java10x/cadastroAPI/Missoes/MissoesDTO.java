7f1c03e279006ec637da3579d31b8c90403b3d7e
Gustavo <l.gustavo.gallo20@gmail.com>
2026-01-07 09:33:46 -0300

    fix: Remove a responsabilidade do Model e passa para o DTO utilizando streams no Mapper

--- src/main/java/dev/java10x/cadastroAPI/Missoes/MissoesDTO.java
+++ src/main/java/dev/java10x/cadastroAPI/Missoes/MissoesDTO.java
@@ -1,4 +1,19 @@
 package dev.java10x.cadastroAPI.Missoes;
 
+import dev.java10x.cadastroAPI.Ninja.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesDTO {
+
+    private Long id;
+    private String nomeMissao;
+    private String dificuldade;
+    private List<NinjaModel> ninjas;
 }
--- src/main/java/dev/java10x/cadastroAPI/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/cadastroAPI/Missoes/MissoesService.java
@@ -4,30 +4,38 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissoesService {
 
     private MissoesRepository missoesRepository;
+    private MissoesMapper missoesMapper;
 
-    public MissoesService(MissoesRepository missoesRepository) {
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
     }
 
     // Listar missoes
-    public List<MissoesModel> listarMissoes() {
-        return missoesRepository.findAll();
+    public List<MissoesDTO> listarMissoes() {
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
     }
 
     // listar missoes por ID
-    public MissoesModel listarMissoesPorId(Long id) {
+    public MissoesDTO listarMissoesPorId(Long id) {
         Optional<MissoesModel> missoesPorId = missoesRepository.findById(id);
-        return missoesPorId.orElse(null);
+        return missoesPorId.map(missoesMapper::map).orElse(null);
     }
 
     // Criar missoes
-    public MissoesModel criarMissoes(MissoesModel missoes) {
-        return missoesRepository.save(missoes);
+    public MissoesDTO criarMissoes(MissoesDTO missoesDTO){
+        MissoesModel missoes = new MissoesMapper().map(missoesDTO);
+        missoes = missoesRepository.save(missoes);
+        return missoesMapper.map(missoes);
     }
 
     // Deletar missoes
@@ -36,10 +44,13 @@ public class MissoesService {
     }
 
     // Alterar nissoes por Id
-    public MissoesModel atualizarMissoes(Long id, MissoesModel missaoAtualizada) {
-        if (missoesRepository.existsById(id)) {
-            missaoAtualizada.setId(id);
-            return missoesRepository.save(missaoAtualizada);
+    public MissoesDTO atualizarMissoes(Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missoesExistente = missoesRepository.findById(id);
+        if (missoesExistente.isPresent()) {
+            MissoesModel missoesAtualizadas = missoesMapper.map(missoesDTO);
+            missoesAtualizadas.setId(id);
+            MissoesModel missoesSalvo = missoesRepository.save(missoesAtualizadas);
+            return missoesMapper.map(missoesSalvo);
         }
         return null;
     }
